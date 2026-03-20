a9e5f932ba42406455543d888832005733019af5
Stepan <pl77sbr@eduvaud.ch>
2026-03-20 12:13:18 +0100

    feat: création des services
    [50] [Wip]

---
+++ P_NutriTrack_Patricny&Reis/Services/DataServices.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace P_NutriTrack_Patricny_Reis.Services
+{
+    public class DataServices
+    {
+        private const string JSON_FILE_NAME = "P_NutriTrack_Patricny&Reis.ServerJson.db.json";
+
+        HttpClient _client;
+        JsonSerializerOptions serializerOptions;
+
+        public List<Aliment> Items { get; private set; }
+
+        public RestService()
+        {
+            _client = new HttpClient();
+            _serializerOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true
+            };
+        }
+    }
+}
