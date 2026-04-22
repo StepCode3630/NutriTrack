7674a4e45277f03a1189826e85e8918a5522a5fb
Stepan <stepanpatricny@gmail.com>
2026-04-22 14:32:35 +0200

    feat: création du service de base
    [40] [Done]

--- P_NutriTrack_Patricny&Reis/Services/DataService.cs
+++ P_NutriTrack_Patricny&Reis/Services/DataService.cs
@@ -1,12 +1,42 @@
-﻿using System;
+﻿using P_NutriTrack_Patricny_Reis.DataModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.JavaScript;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace P_NutriTrack_Patricny_Reis.Services
 {
-    internal class DataService
+    public class DataService
     {
+        private NutriData _data;
+    
+        public async Task LoadData()
+        {
+            using var stream = await FileSystem.OpenAppPackageFileAsync("db.json");
+            using var reader = new StreamReader(stream);
+
+            string json = await reader.ReadToEndAsync();
+
+            _data = JsonSerializer.Deserialize<NutriData>(json);
+        }
+
+        public List<Category> GetCategories()
+        {
+            return _data.Categories;
+        }
+
+        public List<Aliment> GetAliments()
+        {
+            return _data.Aliments; 
+        }
+
+        public List<Aliment> GetAlimentsById(int id)
+        {
+            return _data.Aliments.Where(a => a.CategoryFk == id).ToList();
+        }
     }
+
 }
