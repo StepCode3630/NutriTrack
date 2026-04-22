ec5621a69c8162f1260caf93d90baec03d8c9644
Stepan <stepanpatricny@gmail.com>
2026-04-22 15:05:06 +0200

    feat: ajout du crud des aliments
    [20] [Done]

--- P_NutriTrack_Patricny&Reis/Services/DataService.cs
+++ P_NutriTrack_Patricny&Reis/Services/DataService.cs
@@ -37,6 +37,41 @@ namespace P_NutriTrack_Patricny_Reis.Services
         {
             return _data.Aliments.Where(a => a.CategoryFk == id).ToList();
         }
+        public void addAliment(Aliment a)
+        {
+            // Ajoute un nouvel aliment
+            _data.Aliments.Add(a);
+        }
+
+        public void removeAliment(int id)
+        {
+            // Cherche l'aliment correspondant
+            var aliment = _data.Aliments.FirstOrDefault(a => a.AlimentId == id);
+
+            //Supprime l'aliment si il existe sinon rien
+            if (aliment != null)
+                _data.Aliments.Remove(aliment);
+        }
+
+        public void updateAliment(Aliment alimentUpdated)
+        {
+            // Cherche l'aliment correspondant
+            var aliment = _data.Aliments.FirstOrDefault(a => a.AlimentId == alimentUpdated.AlimentId);
+
+            //Met à jour l'aliment si il existe sinon rien
+            if (aliment != null)
+            {
+                aliment.Name = alimentUpdated.Name;
+                aliment.Calories = alimentUpdated.Calories;
+                aliment.Proteines_g = alimentUpdated.Proteines_g;
+                aliment.Glucides_g = alimentUpdated.Glucides_g;
+                aliment.Lipides_g = alimentUpdated.Lipides_g;
+                aliment.Fibres_g = alimentUpdated.Fibres_g;
+                aliment.Vitamines = alimentUpdated.Vitamines;
+                aliment.Mineraux = alimentUpdated.Mineraux;
+            }
+        }
     }
+
 
 }
