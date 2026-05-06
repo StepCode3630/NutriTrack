0aba90b59254a93a79dbb022c8f38c46e88623be
Stepan <stepanpatricny@gmail.com>
2026-05-06 16:02:11 +0200

    feat: Initialisation de la db
    [20] [Done]

--- P_NutriTrack_Patricny&Reis/App.xaml.cs
+++ P_NutriTrack_Patricny&Reis/App.xaml.cs
@@ -1,15 +1,24 @@
-﻿namespace P_NutriTrack_Patricny_Reis
+﻿using P_NutriTrack_Patricny_Reis.Services;
+
+namespace P_NutriTrack_Patricny_Reis
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
+            InitDatabase();
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
             return new Window(new AppShell());
+        }
+
+        public async void InitDatabase()
+        {
+            var ServiceCollection = new DataService();
+            await ServiceCollection.Init();
         }
     }
 }
