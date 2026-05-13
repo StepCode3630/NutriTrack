a22f831de35cfe4122eddfb08fe8473dcc73f07e
Stepan <stepanpatricny@gmail.com>
2026-05-13 16:31:00 +0200

    feat: création db
    [50] [Done]

---
+++ P_NutriTrack_Patricny&Reis/Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P_NutriTrack_Patricny_Reis.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P_NutriTrack_Patricny_Reis.Data
+{
+    public class AppDbContext : DbContext
+    {
+        private string _databasePath;
+
+        public AppDbContext(string databasePath)
+        { _databasePath = databasePath; }
+
+        public DbSet<Aliment> Aliments { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite();
+        }
+    }
+}
